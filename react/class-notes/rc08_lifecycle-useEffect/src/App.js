11940d8c6b1eb3528152d26acb47326e16f72db9
e2318ismail <67835542+e2318ismail@users.noreply.github.com>
2022-07-02 11:19:01 +0300

    rc8

--- react/class-notes/rc08_lifecycle-useEffect/src/App.js
+++ react/class-notes/rc08_lifecycle-useEffect/src/App.js
@@ -1,9 +1,22 @@
 import LifeCycleMethods from './components/LifeCycleMethods';
+import { useState } from 'react';
+import UseEffectHook from './components/UseEffectHook';
+import Users from './components/Users';
 
 function App() {
+  const [show, setShow] = useState(true);
+
   return (
-    <div className="container text-center mt-4">
-      <LifeCycleMethods />
+    <div className="container text-center" style={{ marginTop: '10rem' }}>
+      {/* <button className="btn btn-dark" onClick={() => setShow(!show)}>
+        {show ? 'Hide' : 'Show'}
+      </button> */}
+
+      {/* Conditional Rendering (Short-Circuit) */}
+      {/* {show && <LifeCycleMethods />} */}
+      {/* {show ? <LifeCycleMethods /> : null} */}
+      {/* {show && <UseEffectHook/>} */}
+      <Users />
     </div>
   );
 }
